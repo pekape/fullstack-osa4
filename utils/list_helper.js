4e582ce8ad96e7f2506d7f870c5733ec587201bd
pekape <37801685+pekape@users.noreply.github.com>
2018-04-25 13:24:01 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return { error: 'empty list' }
+  }
+
+  return blogs.reduce((mostLiked, blog) => {
+    return blog.likes > mostLiked.likes ? blog : mostLiked
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
