1e9e06ba6c951f7c1fd6763f6a4b6de1e2dcac58
pekape <37801685+pekape@users.noreply.github.com>
2018-05-02 13:37:31 +0300

    4.18

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 
 mongoose
@@ -18,6 +19,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const server = http.createServer(app)
 
