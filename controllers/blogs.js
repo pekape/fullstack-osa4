1f6a3a18b63d65b15bee37be59426b3392761f6f
pekape <37801685+pekape@users.noreply.github.com>
2018-05-02 15:17:51 +0300

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -53,6 +53,19 @@ blogsRouter.post('/', async (request, response) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const blog = await Blog.findById(request.params.id)
+
+    if (decodedToken.id.toString() !== blog.user.toString()) {
+      return response.status(401).json({ error: 'blogs can only be removed by their adder' })
+    }
+
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
   } catch (exception) {
--- index.js
+++ index.js
@@ -18,9 +18,7 @@ mongoose
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(middleware.tokenExtractor)
-
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', middleware.tokenExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
